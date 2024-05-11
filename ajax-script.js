f934c6a492a8c228cf76c184e04ef828ee61e83a
zenryui <programmerferg@gmail.com>
2024-05-12 05:41:32 +0800

    ajax script to avoid form for refreshing

---
+++ ajax-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#searchForm").submit(function(event){
+        event.preventDefault(); // Prevent the form from submitting
+
+        var email = $("input[name='email']").val();
+
+        $.ajax({
+            type: "POST",
+            url: "search-email.php",
+            data: {email: email},
+            success: function(response){
+                $("#changePasswordContainer").html(response);
+            }
+        });
+    });
+});
