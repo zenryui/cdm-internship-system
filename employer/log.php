ed9f2b057df5f2b4f5a60fe43c1412a2e9788ba4
Ferg <programmerferg@gmail.com>
2024-05-16 00:27:12 +0800

    profile/resume done for students

--- employer/log.php
+++ employer/log.php
@@ -16,10 +16,27 @@ if (!isset($_SESSION['user_data'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
+    <title>Student Dashboard</title>
 </head>
 <body>
     <h2>Employer Dashboard</h2>
+
+    <!-- Buttons for different actions -->
+    <form action="change_pass.php" method="post">
+        <input type="submit" value="Change Password">
+    </form>
+
+    <form action="profile.php" method="post">
+        <input type="submit" value="Profile Management">
+    </form>
+
+    <form action="post_intern.php" method="post">
+        <input type="submit" value="Post Internship Openings">
+    </form>
+
+    <form action="see_applications.php" method="post">
+        <input type="submit" value="See Applications">
+    </form>
 
     <!-- Button for logout -->
     <form action="logout.php" method="post">
--- students/log.php
+++ students/log.php
@@ -16,10 +16,27 @@ if (!isset($_SESSION['user_data'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
+    <title>Student Dashboard</title>
 </head>
 <body>
     <h2>Student Dashboard</h2>
+
+    <!-- Buttons for different actions -->
+    <form action="change_pass.php" method="post">
+        <input type="submit" value="Change Password">
+    </form>
+
+    <form action="profile.php" method="post">
+        <input type="submit" value="Profile Management">
+    </form>
+
+    <form action="intern_application.php" method="post">
+        <input type="submit" value="Internship Application">
+    </form>
+
+    <form action="notification.php" method="post">
+        <input type="submit" value="Notification">
+    </form>
 
     <!-- Button for logout -->
     <form action="logout.php" method="post">
