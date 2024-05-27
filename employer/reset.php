e8e5251826c778a72b334a3b727fb67c22619773
Ferguzus <programmerferg@gmail.com>
2024-05-27 08:34:39 +0800

    finish

--- employer/reset.php
+++ employer/reset.php
@@ -96,6 +96,6 @@ function showSuccessAlert() {
 </script>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="ajax-script.js"></script>
+<script src="../js/ajax-script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
---
+++ landing-page/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CDM-IRS Landing Page</title>
+    <link rel="stylesheet" href="../assets/css/cdm.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+    <link rel="shortcut icon" href="../assets/img/id-card.png">
+</head>
+<body>
+
+    <!-- HEADER START-->
+    <div class="navbar">
+        <div class="logo">
+            <h1>CDM</h1>
+        </div>
+        
+        <ul class="nav">
+            <li><a href="https://github.com/ferguzus">Developers</a></li>
+            <li><a href="https://github.com/zenryui/cdm-internship-system/tree/fullstack">Github Repository</a></li>
+            <li><a href="#">Resources</a></li>
+        </ul>
+        <ul class="nav">
+            <li><a href="../students/index.php">Login</a></li>
+            <li><a href="../students/signup.php"><button class="button">Sign Up</button></a></li>
+        </ul>
+    </div>
+
+    <!-- MAIN CONTENT START -->
+    <div class="container">
+        <h3>Welcome! to CDM Internship Registration System</h3>
+        <p>Your gateway to professional growth and development.</p>
+        <a href="../students/index.php" class="get-started">Get Started</a>
+    </div>
+</body>
+</html>
