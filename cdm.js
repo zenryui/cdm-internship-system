a00cd86a21a8d3104c4aa84b5e61db74dda0b141
Ferguzus <programmerferg@gmail.com>
2024-05-27 08:36:03 +0800

    well done

--- cdm.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdowns = document.querySelectorAll(".dropdown");
-
-    dropdowns.forEach(function(dropdown) {
-        dropdown.addEventListener("click", function() {
-            this.querySelector(".dropdown-content").classList.toggle("show");
-        });
-    });
-});
--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('toggleButton').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('collapsed');
-  });
-  
